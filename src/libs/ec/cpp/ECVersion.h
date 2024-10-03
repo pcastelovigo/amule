32071a2ee2423a78ce64b275a0d37ec61f2fb1e7
Pablo Castelo <pablocv@gmx.es>
2024-10-03 23:12:25 +0200

    first

---
+++ src/libs/ec/cpp/ECVersion.h
@@ -0,0 +1,12 @@
+/* Automatically generated file. Do not edit! */
+
+#ifndef ECVERSION_H
+#define ECVERSION_H
+
+#include "config.h"
+
+#ifdef SVNDATE
+#define EC_VERSION_ID "c142b79f33158afef8af843108947e23"
+#endif
+
+#endif /* ECVERSION_H */
